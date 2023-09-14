9104c5a12774caa31a3b7f11c459cfd8f13b3888
makbu324 <angusmak@bu.edu>
2023-09-13 21:12:58 -0400

    Added EditText to fill in numbers

--- app/src/main/java/com/example/calcr_hw2_pbm_4/MainActivity.kt
+++ app/src/main/java/com/example/calcr_hw2_pbm_4/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.calcr_hw2_pbm_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //text stuff
+        var n_1 = findViewById<EditText>(R.id.n_1)
+        var n_2 = findViewById<EditText>(R.id.n_2)
+        var result = findViewById<TextView>(R.id.result)
     }
 }
